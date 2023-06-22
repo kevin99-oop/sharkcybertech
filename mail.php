350357b1772ad758c3d2cd63bc5041c367b3529d
kevin99-oop <56681644+kevin99-oop@users.noreply.github.com>
2023-06-22 18:31:52 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$telefon= $_POST['telefon'];
+$website= $_POST['website'];
+$message= $_POST['message'];
+$to = "info@sharkcybertech.com";
+$subject = "Mail From sharkcybertech";
+$txt ="Name = ". $name . "\r\n  Email = " . $email ."telefon = ". $telefon . "\r\n "\r\n "Website = " . $website . "\r\n Message =" . $message;
+$headers = "From: noreply@sharkcybertech.com" . "\r\n" .
+"CC: sharkads02@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+?>
